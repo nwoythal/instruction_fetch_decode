831fe56f586e7ebe829db72a874f01bc07492ccb
Nate W <nwoythal@gmail.com>
2016-02-26 15:35:32 -0700

    Invented spaghetti

--- src/instruction_fetch_decode/Instruction_Fetch_Decode.java
+++ src/instruction_fetch_decode/Instruction_Fetch_Decode.java
@@ -2,6 +2,8 @@ package instruction_fetch_decode;
 import java.io.*;
 import java.util.ArrayList;
 
+/*I've managed to make spaghetti out of this, good luck.*/
+
 public class Instruction_Fetch_Decode 
 {
 	public static int program_counter=0x00400000; //Apparently hex works for integer declarations, which is cool
@@ -15,15 +17,42 @@ public class Instruction_Fetch_Decode
 			"$t8", "$t9", "$k0", "$k1", "$gp", "$sp",
 			"$fp", "$ra"
 	   };
+	public static int[] register_values=new int[32]; //Global so there's no initialization on every get_registers call, initialized with zeroes.
+	public static int RegDst, ALUSrc, MemToReg, RegWrite, Branch, 
+					  PCSrc, ReadAddr1, ReadAddr2, WriteAddr, ALUControl, 
+					  ZeroALU, MemWrite, MemRead, ALUOp;
 	public static void main(String[] args)
 	{
+		String decoded, fetched, binary, output_str="";
 		try
 		{
-			BufferedWriter output=new BufferedWriter(new FileWriter("HW2Disassembly.txt"));
+			BufferedWriter output=new BufferedWriter(new FileWriter("HW2Diagnostics.txt"));
 			Load("HW2MachineCode.txt");
+			output.write("PC,\tInstruction,\tArgs,\tRegDst,\tALUSrc,\tMemToReg,\tRegWrite,\tMemRead,\tMemWrite,\tBranch,\tPCSrc,\tALUOp,\tALUControl,\t$v0,\t$a0,\t$t0"+"\n");
 			for(int i=0;i<operations.size();i++)
 			{
-				output.write(decode(fetch())+"\n"); //Pull machine code instruction from list, translate, then write
+				fetched=fetch();
+				decoded=decode(fetched); //Pull machine code instruction from list, translate
+				fetched=fetched.substring(2);
+				if(decoded=="syscall")
+				{
+					syscall();
+				}
+				else
+				{
+					binary=String.format("%32s", Integer.toBinaryString(Integer.parseInt(fetched, 16))).replace(' ', '0');
+					control(binary);
+					alu_control(binary);
+					ex(decoded.substring(0,5).trim());
+					mem();
+					wb();
+				}
+				output_str="0x"+String.format("%8s", Integer.toHexString(program_counter)).replace(' ', '0')+","+decoded.replaceFirst("\\s", ",");
+				output_str = (output_str.toLowerCase().contains("syscall")) ? (output_str+",0") : (output_str) ;
+				output_str=output_str+",\t"+RegDst+",\t"+ALUSrc+",\t"+MemToReg+",\t"+RegWrite+",\t"+
+						   MemRead+",\t"+MemWrite+",\t"+Branch+",\t"+PCSrc+",\t"+ALUOp+",\t"+
+						   ALUControl+",\t"+register_values[2]+",\t"+register_values[4]+",\t"+register_values[8];
+				output.write(output_str+"\n");
 				program_counter+=4; //Step to next instruction
 			}
 			output.close();
@@ -40,18 +69,19 @@ public class Instruction_Fetch_Decode
 		String instruction="";
 		try
 		{
-			instruction="0x"+String.format("%8s", Integer.toHexString(program_counter)).replace(' ', '0')+"\t";
-			hex=hex.substring(2);
+			instruction="";
+			hex=hex.substring(2); //remove 0x
 			int i=Integer.parseInt(hex, 16);
 			String binary=String.format("%32s", Integer.toBinaryString(i)).replace(' ', '0'); //Ensure when it is translated to binary, we don't lose precision
 			String binary_op=binary.substring(0, 6);
+			ReadAddr2=Integer.parseInt(get_jump_loc(binary.substring(6)).substring(2), 16);
 			switch(Integer.parseInt(binary_op, 2)) //Op code lookup table
 			{
 				//R-Types
 				case 0:
 					try
 					{
-						return instruction+get_function(binary.substring(6));
+						instruction=instruction+get_function(binary.substring(6));
 					}
 					catch(InvalidOpException ex)
 					{
@@ -60,78 +90,85 @@ public class Instruction_Fetch_Decode
 					break;
 				//J-Types
 				case 2:
-					instruction=instruction+"j\t";
-					instruction=instruction+get_jump_loc(binary.substring(6));
-					return instruction;
+					instruction=instruction+"j ";
+					instruction=instruction+ReadAddr2;
+					program_counter=ReadAddr2-4;
+					ReadAddr2=0;
+					break;
 				case 3:
-					instruction=instruction+"jal\t";
-					instruction=instruction+get_jump_loc(binary.substring(6));
-					return instruction;
+					instruction=instruction+"jal ";
+					instruction=instruction+ReadAddr2;
+					ReadAddr1=program_counter;
+					program_counter=ReadAddr2-4;
+					ReadAddr2=0;
+					break;
 				//I-Types
 				case 4:
-					instruction=instruction+"beq\t";
-					instruction=instruction+get_reg(binary.substring(6, 11))+get_reg(binary.substring(11,16));
-					return instruction+calculate_imm(binary.substring(18));
+					ReadAddr1=get_reg(binary.substring(11,16));
+					ReadAddr2=get_reg(binary.substring(6, 11));
+					instruction=instruction+"beq "+register_list[ReadAddr2]+" "+register_list[ReadAddr1];
+					instruction=instruction+" "+calculate_imm(binary.substring(18));
+					break;
 				case 5:
-					instruction=instruction+"bne\t";
-					instruction=instruction+get_reg(binary.substring(6, 11))+get_reg(binary.substring(11,16));
-					return instruction+calculate_imm(binary.substring(18));
+					ReadAddr1=get_reg(binary.substring(6, 11));
+					ReadAddr2=get_reg(binary.substring(11,16));
+					instruction=instruction+"bne "+register_list[ReadAddr2]+" "+register_list[ReadAddr1];
+					instruction=instruction+" "+calculate_imm(binary.substring(18));
+					break;
 				case 8:
-					instruction=instruction+"addi\t";
+					ReadAddr1=get_reg(binary.substring(11, 16));
+					ReadAddr2=get_reg(binary.substring(6,11));
+					WriteAddr=program_counter+4*Integer.parseInt(calculate_imm(binary.substring(18)));
+					instruction=instruction+"addi "+register_list[ReadAddr1]+" "+register_list[ReadAddr2];
+					instruction=instruction+" "+calculate_imm(binary.substring(18));
 					break;
 				case 9:
-					instruction=instruction+"addiu\t";
+					instruction=instruction+"addiu ";
 					break;
 				case 10:
-					instruction=instruction+"slti\t";
+					instruction=instruction+"slti ";
 					break;
 				case 11:
-					instruction=instruction+"sltiu\t";
+					instruction=instruction+"sltiu ";
 					break;
 				case 12:
-					instruction=instruction+"andi\t";
+					instruction=instruction+"andi ";
 					break;
 				case 13:
-					instruction=instruction+"ori\t";
+					instruction=instruction+"ori ";
 					break;
 				case 14:
-					instruction=instruction+"xori\t";
+					instruction=instruction+"xori ";
 					break;
 				case 15:
-					instruction=instruction+"lui\t";
+					instruction=instruction+"lui ";
 					break;
 				case 32:
-					instruction=instruction+"lb\t";
+					instruction=instruction+"lb ";
 					break;
 				case 33:
-					instruction=instruction+"lh\t";
+					instruction=instruction+"lh ";
 					break;
 				case 35:
-					instruction=instruction+"lw\t";
+					instruction=instruction+"lw ";
 					break;
 				case 36:
-					instruction=instruction+"lbu\t";
+					instruction=instruction+"lbu ";
 					break;
 				case 37:
-					instruction=instruction+"lhu\t";
+					instruction=instruction+"lhu ";
 					break;
 				case 40:
-					instruction=instruction+"sb\t";
+					instruction=instruction+"sb ";
 					break;
 				case 41:
-					instruction=instruction+"sh\t";
+					instruction=instruction+"sh ";
 					break;
 				case 43:
-					instruction=instruction+"sw\t";
+					instruction=instruction+"sw ";
 					break;
 				default:
 					throw new InvalidOpException("No corresponding MIPS instruction!");
-			}
-			instruction=instruction+get_reg(binary.substring(11, 16))+get_reg(binary.substring(6,11));
-			instruction=instruction+calculate_imm(binary.substring(18));
-			if(instruction.charAt(instruction.length()-2)==',')
-			{
-				instruction=instruction.substring(0, instruction.length()-2); //Remove trailing commas
 			}
 		}
 		catch(InvalidOpException ex)
@@ -182,89 +219,79 @@ public class Instruction_Fetch_Decode
 		switch(Integer.parseInt(funct_code.substring(20), 2)) //function code lookup table
 		{
 			case 0:
-				func="sll\t";
+				func="sll ";
 				break;
 			case 2:
-				func="srl\t";
+				func="srl ";
 				break;
 			case 3:
-				func="sra\t";
+				func="sra ";
 				break;
 			case 4:
-				func="sllv\t";
+				func="sllv ";
 				break;
 			case 6:
-				func="srlv\t";
+				func="srlv ";
 				break;
 			case 7:
-				func="srav\t";
+				func="srav ";
 				break;
 			case 8:
-				func="jr\t"+get_reg(funct_code.substring(0,5));
-				func=func.substring(0, func.length()-2); //Trim trailing comma
-				return func;
+				func="jr ";
+				WriteAddr=get_reg(funct_code.substring(0,5));
+				func=func+" "+register_list[WriteAddr];
+				break;
 			case 9:
-				func="jalr\t";
+				func="jalr ";
 				break;
 			case 12:
 				return "syscall"; //there are no requirements, so we return
-				//break;
 			case 13:
 				return "break"; //there are no requirements, so we return
-				//break;
 			case 32:
-				func="add\t";
+				func="add ";
+				WriteAddr = get_reg(funct_code.substring(10, 15));
+				ReadAddr1 = get_reg(funct_code.substring(0, 5));
+				ReadAddr2 = get_reg(funct_code.substring(5, 10));
+				func=func+register_list[WriteAddr]+" "+register_list[ReadAddr1]+" "+register_list[ReadAddr2]; //Decode all three registers
 				break;
 			case 33:
-				func="addu\t";
+				func="addu ";
 				break;
 			case 34:
-				func="sub\t";
+				func="sub ";
 				break;
 			case 35:
-				func="subu\t";
+				func="subu ";
 				break;
 			case 36:
-				func="and\t";
+				func="and ";
 				break;
 			case 37:
-				func="or\t";
+				func="or ";
 				break;
 			case 38:
-				func="xor\t";
+				func="xor ";
 				break;
 			case 39:
-				func="nor\t";
+				func="nor ";
 				break;
 			case 42:
-				func="slt\t";
+				func="slt ";
 				break;
 			case 43:
-				func="sltu\t";
+				func="sltu ";
 				break;
 			default:
 				throw new InvalidOpException("No corresponding MIPS instruction!");
-		}
-		func=func+get_reg(funct_code.substring(10, 15))+get_reg(funct_code.substring(0, 5))+get_reg(funct_code.substring(5, 10)); //Decode all three registers
-		if(func.charAt(func.length()-2)==',')
-		{
-			func=func.substring(0, func.length()-2); //remove trailing comma
 		}
 		return func;
 	}
 	
 	//Change binary register address to valid register, using global array
-	public static String get_reg(String rd) throws InvalidOpException
+	public static int get_reg(String rd)
 	{
-		try
-		{
-			rd=register_list[Integer.parseInt(rd,2)]+", ";
-		}
-		catch(ArrayIndexOutOfBoundsException ex)
-		{
-			throw new InvalidOpException("Register address is not valid!");
-		}
-		return rd;
+		return Integer.parseInt(rd,2);
 	}
 	
 	//Change binary immediate value to decimal
@@ -272,6 +299,238 @@ public class Instruction_Fetch_Decode
 	{
 		Integer fromString=new Integer(Integer.parseInt(immediate, 2));
 		return Integer.toString(fromString.byteValue()); //convert to signed 8-bit value
+	}
+	
+	public static void control(String instruction)
+	{
+		RegDst = ALUSrc = MemToReg = RegWrite = Branch = PCSrc = 0;
+		switch(Integer.parseInt(instruction.substring(0, 8), 2))
+		{
+			case 0:
+				switch(Integer.parseInt(instruction.substring(26), 2))
+				{
+				case 8:
+					PCSrc=3;
+					break;
+				case 32:
+					RegDst=RegWrite=1;
+					break;
+				default: //captures syscall
+					break;
+				}
+				break;
+			case 2:
+				PCSrc=2;
+				break;
+			case 3:
+				RegDst=2;
+				MemToReg=2;
+				RegWrite=1;
+				PCSrc=2;
+				break;
+			case 4:
+				Branch=1;
+				PCSrc=ZeroALU;
+				break;
+			case 8:
+				ALUSrc=1;
+				RegWrite=1;
+				break;
+			default:
+				break;
+		}
+	}
+	
+	//id() was integrated with decode()
+	
+	public static void alu_control(String instruction)
+	{
+		String fct="";
+		String Op=instruction.substring(0, 6);
+		switch(Integer.parseInt(Op,2))
+		{
+			case 0:
+				fct=instruction.substring(26);
+				switch(Integer.parseInt(fct,2))
+				{
+					case 0:
+						ALUControl=3;
+						break;
+					case 2:
+						ALUControl=4;
+						break;
+					case 3:
+						ALUControl=5;
+						break;
+					case 4:
+						ALUControl=3;
+						break;
+					case 6:
+						ALUControl=4;
+						break;
+					case 7:
+						ALUControl=5;
+						break;
+					case 32:
+						ALUControl=2;
+						break;
+					case 33:
+						ALUControl=2;
+						break;
+					case 34:
+						ALUControl=6;
+						break;
+					case 35:
+						ALUControl=6;
+						break;
+					case 36:
+						ALUControl=0;
+						break;
+					case 37:
+						ALUControl=1;
+						break;
+					case 38:
+						ALUControl=9;
+						break;
+					case 39:
+						ALUControl=8;
+						break;
+					case 42:
+						ALUControl=7;
+						break;
+					case 43:
+						ALUControl=7;
+						break;
+					default:
+						break;
+				}
+			case 4:
+				ALUControl=6;
+				break;
+			case 5:
+				ALUControl=6;
+				break;
+			case 8:
+				ALUControl=2;
+				break;
+			case 9:
+				ALUControl=2;
+				break;
+			case 10:
+				ALUControl=7;
+				break;
+			case 11:
+				ALUControl=7;
+				break;
+			case 12:
+				ALUControl=0;
+				break;
+			case 13:
+				ALUControl=1;
+				break;
+			case 14:
+				ALUControl=9;
+				break;
+			case 15:
+				ALUControl=3;
+				break;
+			case 32:
+				ALUControl=2;
+				break;
+			case 33:
+				ALUControl=2;
+				break;
+			case 35:
+				ALUControl=2;
+				break;
+			case 36:
+				ALUControl=2;
+				break;
+			case 37:
+				ALUControl=2;
+				break;
+			case 40:
+				ALUControl=2;
+				break;
+			case 41:
+				ALUControl=2;
+				break;
+			case 43:
+				ALUControl=2;
+				break;
+			default:
+				break;
+			}
+		ALUOp=ALUControl;
+	}
+	
+	public static void ex(String op)
+	{
+		switch(ALUControl)
+		{
+			case 1:
+				ZeroALU=ReadAddr1 & ReadAddr2;
+				break;
+			case 2:
+				ZeroALU=ReadAddr1 | ReadAddr2;
+				break;
+			case 3:
+				ZeroALU=ReadAddr1 + ReadAddr2;
+				break;
+			case 4:
+				ZeroALU = ReadAddr1 << 10;
+				break;
+			case 5:
+				ZeroALU = ReadAddr1 >> 10;
+				break;
+			case 6:
+				ZeroALU=ReadAddr1-ReadAddr2;
+				ZeroALU = (op=="beq"&&ZeroALU==0) ? 1 : 0;
+				ZeroALU = (op=="bne"&&ZeroALU!=0) ? 1 : 0;
+				if(ZeroALU==1)
+				{
+					program_counter+=4*ReadAddr2;
+				}
+				break;
+			case 7:
+				//slt
+				break;
+			case 8:
+				//nor
+				break;
+			case 9:
+				//xor
+				break;
+			default:
+				//shouldn't get here, ever
+				break;
+		}
+	}
+	
+	public static void mem()
+	{
+		//does nothing
+	}
+	
+	public static void wb()
+	{
+		if(RegWrite==1)
+		{
+			//register_values[WriteAddr]=register_values[ReadAddr1]+register_values[ReadAddr2]; //OutOfBoundsException here
+		}
+	}
+	
+	public static void syscall()
+	{
+		if (register_values[2]==10)
+		{
+			System.out.println("Program terminated");
+			System.exit(0); //Terminate program
+		}
+		else if(register_values[2]==10)
+		{
+			System.out.println(register_values[4]);
+		}
 	}
 }
 
